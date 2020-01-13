4b9fdac53448f24120bdcc51aba33d99eaab935c
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-13 16:38:33 -0500

    make /data/db mine

--- data/db.js
+++ data/db.js
@@ -1,6 +1,7 @@
-const knex = require('knex');
-const knexConfig = require('../knexfile.js');
-const db = knex(knexConfig.development);
+const _ = require ('lodash')
+const knex = require ('knex')
+const knexConfig = require ('../knexfile.js')
+const db = knex (knexConfig.development)
 
 module.exports = {
   find,
@@ -8,32 +9,49 @@ module.exports = {
   insert,
   update,
   remove,
-};
-
-function find() {
-  return db('users');
 }
 
-function findById(id) {
-  return db('users')
-    .where({ id: Number(id) })
-    .first();
+function find (id = undefined) {
+  if (_.isNil (id)) {
+    return (
+      db ('users')
+    )
+  }
+  else {
+    return (
+      findById (id)
+    )
+  }
 }
 
-function insert(user) {
-  return db('users')
-    .insert(user)
-    .then(ids => ({ id: ids[0] }));
+function findById (id) {
+  return (
+    db ('users')
+      .where ({ id: Number (id) })
+      .first ()
+  )
 }
 
-function update(id, user) {
-  return db('users')
-    .where('id', Number(id))
-    .update(user);
+function insert (user) {
+  return (
+    db ('users')
+      .insert (user)
+      .then (ids => ({ id: ids[0] }))
+  )
 }
 
-function remove(id) {
-  return db('users')
-    .where('id', Number(id))
-    .del();
+function update (id, user) {
+  return (
+    db ('users')
+      .where ('id', Number (id))
+      .update (user)
+  )
+}
+
+function remove (id) {
+  return (
+    db ('users')
+      .where ('id', Number (id))
+      .del ()
+  )
 }
