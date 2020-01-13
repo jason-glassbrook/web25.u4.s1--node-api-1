5bb9466dc4dc9bc53e74927fd36ec69b5af2a33b
Jason Glassbrook <28660371+jason-glassbrook@users.noreply.github.com>
2020-01-13 17:30:23 -0500

    init server, db

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// implement your API here
+/// external modules ///
+const _ = require ('lodash/fp')
+const express = require ('express')
+
+/// database ///
+const db = {
+  users : require ('./data/db.js'),
+}
+
+/// server ///
+const port = 5555
+const server = express ()
+server.use (express.json ())
+
